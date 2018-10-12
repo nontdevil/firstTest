2ba37cbaa092708b06429933a996f4f7d54ca3ef
Nont <nontnutchanon@gmail.com>
2018-10-12 11:10:24 +0700

    first

--- gitunit.py
+++ gitunit.py
@@ -1,1 +1,11 @@
 #clean start
+import pyglet
+import glooey
+
+mainWindow = pyglet.window.Window()
+mainGui = glooey.Gui(mainWindow)
+
+label = glooey.Label("Hello World")
+mainGui.add(label)
+
+pyglet.app.run()
