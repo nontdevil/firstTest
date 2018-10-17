3baf7495fd33f5746963dadddb259843d365f30a
Nont <nontnutchanon@gmail.com>
2018-10-17 10:52:11 +0700

    passwordchecker

--- gitunit.py
+++ gitunit.py
@@ -6,17 +6,18 @@ import glooey
 mainWindow = pyglet.window.Window()
 mainGui = glooey.Gui(mainWindow)
 rows = glooey.VBox()
-
+mainGui.add(rows)
 title = glooey.Label("Welcome to Password Checker")
-enterPassword = glooey.Form("Enter your password: ")
+enterPassword = glooey.Form()
 button = glooey.Button("Check Password")
 result = glooey.Label("")
 
 def buttonClicked(widget):
-    if enterPassword.text == "1234" or enterPassword.text == "123456" or enterPassword.text == "12345678" or enterPassword.text == "password" or enterPassword == "qwerty":
+    if enterPassword.text == "1234" or enterPassword.text == "123456" or enterPassword.text == "12345678" or enterPassword.text == "password" or enterPassword.text == "qwerty":
         result.set_text("Your password is too weak!")
     else:
         result.set_text("Your password is ok")
+    
 
 
 rows.add(title)
