0361ce2ce83f1636a9bd1b622e8a5d54401f9745
Nont <nontnutchanon@gmail.com>
2018-10-17 10:33:17 +0700

    asdfghjkl;'

--- gitunit.py
+++ gitunit.py
@@ -4,8 +4,27 @@ import glooey
 
 mainWindow = pyglet.window.Window()
 mainGui = glooey.Gui(mainWindow)
-
+rows = glooey.VBox()
+col = glooey.HBox()
+mainGui.add(rows)
+backgroud = "#FF0000"
 label = glooey.Label("Hello World")
-mainGui.add(label)
+label2 = glooey.Label("Hello Banana")
+label3 = glooey.Label("Hello Cantaloupe")
+
+form = glooey.Form("Enter password: ")
+
+button = glooey.Button("Click Here!")
+
+
+def buttonClicked(widget):
+    print("Clicked!")
+button.push_handlers(on_click=buttonClicked)
+
+rows.add(button)
+rows.add(form)
+rows.add(label)
+rows.add(label2)
+rows.add(label3)
 
 pyglet.app.run()
