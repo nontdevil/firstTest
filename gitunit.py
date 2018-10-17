c00bbd08dc89701aae211a6aa2fce4fa8558c3e8
Nont <nontnutchanon@gmail.com>
2018-10-17 10:46:43 +0700

    l;'

--- gitunit.py
+++ gitunit.py
@@ -1,37 +1,28 @@
 #clean start
-
-"""
 import pyglet
 import glooey
 
+#passwordChecker
 mainWindow = pyglet.window.Window()
 mainGui = glooey.Gui(mainWindow)
 rows = glooey.VBox()
-col = glooey.HBox()
-mainGui.add(rows)
-background = "#FF0000"
-label = glooey.Label("Hello World")
-label2 = glooey.Label("Hello Banana")
-label3 = glooey.Label("Hello Cantaloupe")
 
-form = glooey.Form("Enter password: ")
-
-button = glooey.Button("Click Here!")
-
+title = glooey.Label("Welcome to Password Checker")
+enterPassword = glooey.Form("Enter your password: ")
+button = glooey.Button("Check Password")
+result = glooey.Label("")
 
 def buttonClicked(widget):
-    print(form.text)
+    if enterPassword.text == "1234" or enterPassword.text == "123456" or enterPassword.text == "12345678" or enterPassword.text == "password" or enterPassword == "qwerty":
+        result.set_text("Your password is too weak!")
+    else:
+        result.set_text("Your password is ok")
 
 
+rows.add(title)
+rows.add(enterPassword)
+rows.add(button)
+rows.add(result)
 button.push_handlers(on_click=buttonClicked)
 
-rows.add(button)
-rows.add(form)
-rows.add(label)
-rows.add(label2)
-rows.add(label3)
-
 pyglet.app.run()
-"""
-
-#passwordChecker
